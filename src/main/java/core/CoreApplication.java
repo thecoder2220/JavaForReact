fcb022b3a9d01a452d2a66b9e06f77954a4bf403
CARL <fathera6@gmail.com>
2017-06-10 00:00:34 +0200

    run spring

---
+++ src/main/java/core/CoreApplication.java
@@ -0,0 +1,18 @@
+package core;
+
+/**
+ * Created by CARL on 09/06/2017.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+@EnableAutoConfiguration
+@SpringBootApplication
+public class CoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CoreApplication.class, args);
+    }
+}
